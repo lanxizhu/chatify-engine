b39e93efd71f395555a4a27d5d88daa70a9d581b
lanxiz <mr.longzhentian@gmail.com>
2025-03-30 20:16:02 +0800

    chore: add database sql

---
+++ resources/data/chatify.sql
@@ -0,0 +1,28 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80041 (8.0.41)
+ Source Host           : localhost:3306
+ Source Schema         : chatify
+
+ Target Server Type    : MySQL
+ Target Server Version : 80041 (8.0.41)
+ File Encoding         : 65001
+
+ Date: 26/03/2025 20:37:14
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for chatify
+-- ----------------------------
+DROP database if EXISTS chatify;
+CREATE database chatify CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+SET FOREIGN_KEY_CHECKS = 1;
