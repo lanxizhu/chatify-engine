ada7b0e10d263e4edb9fec1e674df45c1cbe6946
lanxiz <mr.longzhentian@gmail.com>
2025-05-19 21:11:09 +0800

    feat: add friend_request table

--- resources/data/friend_request.sql
+++ resources/data/friend_request.sql
@@ -11,29 +11,32 @@
  Target Server Version : 80041 (8.0.41)
  File Encoding         : 65001
 
- Date: 17/05/2025 20:16:36
+ Date: 19/05/2025 20:31:10
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for friends
+-- Table structure for friend_request
 -- ----------------------------
-DROP TABLE IF EXISTS `friends`;
-CREATE TABLE `friends`  (
-  `id` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT (UUID()),
-  `mine` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+DROP TABLE IF EXISTS `friend_request`;
+CREATE TABLE `friend_request`  (
+  `id` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT (UUID()),
+  `user` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `friend` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'request',
+  `created_at` timestamp NULL DEFAULT NULL DEFAULT (NOW()),
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  FOREIGN KEY (`mine`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (`friend`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of friends
+-- Records of friend_request
 -- ----------------------------
-INSERT INTO `friends` VALUES ('e497f609-331d-11f0-8dfc-e454e83a8e0d', '3553028d-a33b-42a7-9cd7-7a9b0c880192', 'aff2c966-7870-463e-a245-01e669ba8722', 'Insert by database.');
+INSERT INTO `friend_request` VALUES ('820734d2-4e92-49f6-bf06-8668a62882ce', '3553028d-a33b-42a7-9cd7-7a9b0c880192', 'aff2c966-7870-463e-a245-01e669ba8722', 'request remark', 'request',null,null);
 
 SET FOREIGN_KEY_CHECKS = 1;
